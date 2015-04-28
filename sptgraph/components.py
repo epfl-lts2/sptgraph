21c6d5cd46fcb8c7ddbe8e05b9af20022733b212
Kikohs <kirell.benzi@epfl.ch>
2015-04-28 18:14:30 +0200

    Component to networkx graph

--- sptgraph/components.py
+++ sptgraph/components.py
@@ -2,6 +2,7 @@
 
 import graphlab as gl
 import numpy as np
+import networkx as nx
 
 
 def find_connected_components(g):
@@ -27,18 +28,40 @@ def get_component_sframe(g, baseid_name='baseID', layer_name='layer'):
     comps['width'] = comps.apply(lambda x: len(np.unique(x[layers])))
     comps['height'] = comps.apply(lambda x: len(np.unique(x[baseids])))
 
-    return comps.sort('size', False)
+    return comps.sort('node_count', False)
 
 
-def extract_components(g, comp_sframe, min_size=2):
+def extract_components(g, comp_sframe, min_node_count=2):
     comps = []
     nodes = g.vertices
     edges = g.edges
-    cc = comp_sframe[comp_sframe['size'] > min_size]
+    cc = comp_sframe[comp_sframe['node_count'] > min_node_count]
     for cid in cc['component_id']:
         n = nodes[nodes['component_id'] == cid]
         e = edges[edges['component_id'] == cid]
         c = gl.SGraph(n, e)
         comps.append(c)
     return comps
+
+
+def component_to_networkx(comp, h, layer_to_ts=None):
+    g = nx.DiGraph()
+    g.name = 'Component ' + str(comp['component_id'])
+
+    if layer_to_ts is not None:
+        for i in xrange(comp['node_count']):
+            g.add_node(comp['nodes'][i],
+                      {'base_id': comp['base_ids'][i], 'layer': comp['layers'][i],
+                       'timestamp': layer_to_ts[comp['layers'][i]].strftime("%Y-%m-%d %H:%M:%S"),
+                       'label': comp['base_ids'][i]
+                      })
+    else:
+        for i in xrange(comp['node_count']):
+            g.add_node(comp['nodes'][i], {'base_id': comp['base_ids'][i], 'layer': comp['layers'][i]})
+
+    edges = h.edges[h.edges['component_id'] == comp['component_id']][['__src_id', '__dst_id']]
+
+    for k in edges:
+        g.add_edge(k['__src_id'], k['__dst_id'])
+    return g
 
